504c12585b893801229538808c07903e272d0c31
hashimpublic-tech <hashim.public@gmail.com>
2026-01-29 00:27:49 +0500

    if_else_if_day_2
    its my c++ journey , I hope it will be good , and i will also give it my best and never break my consestency

---
+++ if_else_if _stat_2/if_ else _st_2.cpp
@@ -0,0 +1,83 @@
+//#include<iostream>
+//using namespace std ;
+
+/*int main(){
+    int marks ;
+    cout << " enter your marks :"<< endl;
+    cin >> marks ;
+
+    if (marks >= 80){
+        cout << " your grade is A : "<< endl;
+    }else if (marks >= 70){
+        cout << " your grade is B :"<< endl;
+    }else if (marks >= 60){
+        cout << " your grade is C :" << endl;
+    }else if (marks >= 50){
+        cout << " your grade is D :"<< endl;
+    } else {
+        cout << " you are fail : "<< endl;
+    } return 0 ;*/
+
+
+/*#include<iostream>
+using namespace std ;
+
+int main (){
+    int score ;
+    int sixes ;
+
+    cout << " enter player score :"<< endl;
+    cin >> score ;
+    cout << " enter sixes : "<< endl;
+    cin >> sixes ;
+
+    if (score >= 50){
+        if(sixes >=3)
+        cout << " exellent batting : "<< endl;
+    }
+    else if (score >= 50){
+        if(sixes <= 3)
+         cout << " good batting :";
+    }
+    else {
+    cout << " needs improvement :";
+}
+    return 0 ;
+}*/
+
+
+
+/*#include<iostream>
+#include <string>
+using namespace std;
+
+int main (){
+    int age ;
+    char licence ;
+
+    cout << " enter your age : "<< endl;
+    cin >> age ;
+    cout << " do you have driving licence :"<< endl;
+    cin >> licence ;
+ 
+    if (age >= 18){
+        if( licence=='Y')
+        cout <<" you are eligibal to drive :"<< endl;
+    }
+    else if (age >= 18){
+        if (licence == 'N')
+        cout << " not allowed , first you need to get a licence :"<< endl;
+    } 
+    else{ cout << "you are not eligibal to drive :";} 
+}*/
+#include<iostream>
+using namespace std ;
+int main(){
+     long int age ;
+     
+     cout << sizeof(short int)<< endl;
+     return 0;
+}
+
+
+
