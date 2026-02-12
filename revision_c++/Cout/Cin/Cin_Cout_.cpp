7b3f11e1e7ae0dd5b390dca083a42c21fb17cc6f
hashimpublic-tech <hashim.public@gmail.com>
2026-02-12 19:09:54 +0500

    cout_practice_.cpp
    how to take numbers  in input from user and do their sum

---
+++ revision_c++/Cout/Cin/Cin_Cout_.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+using namespace std ;
+int main(){
+    int a ; 
+    int b ;
+
+    cout << " enter a number : " ;
+    cin >> a ;
+    cout << " enter a number : " ;
+    cin >> b ;
+
+    int sum = a+b ;
+
+    system( " cls ") ;
+
+    cout << "\nsum of numbers is : " << sum << " \n " << endl;
+
+    return 0 ;
+}
