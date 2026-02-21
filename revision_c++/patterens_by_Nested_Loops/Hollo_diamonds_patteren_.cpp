22293ef8fe23ad887086453324e75b71caf471b3
hashimpublic-tech <hashim.public@gmail.com>
2026-02-21 23:26:37 +0500

    hollow_diamond_patren_ptactice_.cpp

--- revision_c++/patterens_by_Nested_Loops/Hollo_diamonds_patteren_.cpp
+++ revision_c++/patterens_by_Nested_Loops/Hollo_diamonds_patteren_.cpp
@@ -1,7 +1,7 @@
 #include<iostream>
 using namespace std ;
 int main(){
-    int n = 5 ;
+    int n = 4 ;
 
     for( int i=0 ; i<n ; i++ ){
         for( int j=0 ; j<n-i ; j++ ){
@@ -39,4 +39,20 @@ int main(){
         }
         cout << endl;
     }
+    return 0 ;
 }
+
+// hollo diamond pattren :
+
+
+// output :       
+//           * * 
+//         *     * 
+//       *         * 
+//     *             * 
+//   *                 * 
+//   *                 * 
+//     *             * 
+//       *         * 
+//         *     * 
+//           * * 
